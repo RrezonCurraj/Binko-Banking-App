7fc984d3722037019bfab24eddd6d1cbc4e58a73
Rrezon Curraj <rrezoncurraj10@gmail.com>
2025-04-22 23:37:39 +0200

    implement sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const looggedIn = { firstName: "Rrezon" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={looggedIn.firstName || "Guest"}
+            subtext="Acces and manage ur accoun and transactions "
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={[1]}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import { sidebarLinks } from "@/constants";
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Binko Logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">BINKO</h1>
+        </Link>
+        {sidebarLinks.map((item) => {
+          return (
+            <Link href={item.route} key={item.label}>
+              {item.label}
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default Sidebar;
