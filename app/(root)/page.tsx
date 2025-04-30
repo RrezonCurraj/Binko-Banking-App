69dfd0e35b220db31ef84c920e3375ea7ba95f8b
Rrezon Curraj <rrezoncurraj10@gmail.com>
2025-04-30 12:17:34 +0200

    Initial commit for right sidebar (no logic yet)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
 import HeaderBox from "@/components/HeaderBox";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/ui/RightSidebar";
 
 const Home = () => {
-  const looggedIn = { firstName: "Rrezon" };
+  const loggedIn = {
+    firstName: "Rrezon",
+    lastName: "Curraj",
+    email: "rrezoncurraj10@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -10,16 +15,18 @@ const Home = () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={looggedIn.firstName || "Guest"}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Acces and manage ur accoun and transactions "
           />
           <TotalBalanceBox
             accounts={[]}
-            totalBanks={[1]}
+            totalBanks={1}
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSATIONS
       </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
     </section>
   );
 };
